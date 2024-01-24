7185369b037156213611c9eaef6b9d09e470e68a
mesfint <mesfin_meng@yahoo.com>
2024-01-24 23:51:12 +0200

    add cors

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // It parse url, when we create parameters etc
 
